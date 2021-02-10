9211f8a9e1ccf162bbed9cdbdaa7231c7a75a92a
Jonathan Pritchard <jpritcha3.14@gmail.com>
2021-02-10 14:53:46 -0800

    reset game when avatar is clicked

--- minesweeper.py
+++ minesweeper.py
@@ -7,32 +7,11 @@ from itertools import product
 from collections import deque
 from PIL import ImageTk, Image
 
-# Size and number of mines, to be replaced with parseargs
-sz = 20 
-b = 50
-
-# Color and style config
-clickedstyle="sunken"
-clickedcolor="gray80"
-unclickedstyle="raised"
-unclickedcolor="gray65"
-font="consolas 10 bold"
-numcolor = {1:"blue", 2:"green", 3:"red", 4:"purple", 5:"yellow", 6:"turquoise3", 7:"black", 8:"lightskyblue4"}
-
-# Frame setup
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack(side="bottom")
-status = tk.Frame(root)
-status.pack(side="top")
-
-tiles = [[] for _ in range(sz)]
-
 class Tile:
-    def __init__(self, row, col, ismine):
+
+    def __init__(self, row, col):
         self.row = row
         self.col = col
-        self.ismine = ismine 
         self.neighbormines = -1
         self.flagged = False
         self.clicked = False
@@ -45,6 +24,9 @@ class Tile:
         self.label.bind("<Button-1>", self.anticipate)
         self.label.bind("<ButtonRelease-2>", self.right_click)
         self.label.bind("<ButtonRelease-3>", self.right_click)
+
+    def ismine(self):
+        return (self.row * sz + self.col) in mines
 
     @staticmethod
     def inbounds(tile, i, j):
@@ -63,7 +45,7 @@ class Tile:
             for i, j in product([-1, 0, 1], [-1, 0, 1]):
                 if self.inbounds(cur, i, j):
                     t = tiles[cur.row + i][cur.col + j]
-                    if t.ismine:
+                    if t.ismine():
                         minecount += 1
                     else:
                         if not t.clicked and t.neighbormines == -1:
@@ -96,7 +78,7 @@ class Tile:
             if self.inside(event.x, event.y):
                 self.clicked = True
                 self.label.config(relief=clickedstyle, bg=clickedcolor)
-                if self.ismine:
+                if self.ismine():
                     avatar.config(image=dead)
                     for r, c in map(lambda b: (b // sz, b % sz), mines):
                         tiles[r][c].label.configure(fg="black", bg="red")
@@ -106,24 +88,65 @@ class Tile:
                     self.checkneighbors()
 
     def right_click(self, event=None):
-        print(self.label.winfo_height(), self.label.winfo_width(), event.y, event.x)
         if not self.clicked and self.inside(event.x, event.y):
             self.flagged = not self.flagged
             if self.flagged:
                 self.text.set("!")
             else:
                 self.text.set(" ")
+                
+def restart_game(event=None):
+    # Reset Avatar
+    avatar.config(image=fine)
 
+    # Create a random 1D set of mines that maps to the 2D tile grid
+    # These are managed independently of individual tiles, with the
+    # self.ismine() function used for lookup
+    mines.clear()
+    while len(mines) < b:
+        mines.add(random.randint(0,sz**2-1))
 
-# Create a random 1D set of mines that maps to the 2D tile grid
+    print('here')
+
+    # Reset all tiles 
+    for row in range(sz):
+        for col in range(sz):
+            tiles[row][col].text.set(" ")
+            tiles[row][col].label.config(fg="red", relief=unclickedstyle, bg=unclickedcolor)
+            tiles[row][col].row = row
+            tiles[row][col].col = col
+            tiles[row][col].neighbormines = -1
+            tiles[row][col].flagged = False
+            tiles[row][col].clicked = False
+
+    root.mainloop()
+    
+# Size and number of mines, to be replaced with parseargs
+sz = 20 
+b = 50
+
+# Color and style config
+clickedstyle="sunken"
+clickedcolor="gray80"
+unclickedstyle="raised"
+unclickedcolor="gray65"
+font="consolas 10 bold"
+numcolor = {1:"blue", 2:"green", 3:"red", 4:"purple", 5:"yellow", 6:"turquoise3", 7:"black", 8:"lightskyblue4"}
+# Frame setup
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack(side="bottom")
+status = tk.Frame(root)
+status.pack(side="top")
+
+tiles = [[] for _ in range(sz)]
 mines = set()
-while len(mines) < b:
-    mines.add(random.randint(0,sz**2-1))
 
 # Create the tile grid
 for row in range(sz):
     for col in range(sz):
-        cur_tile = Tile(row, col, (row * sz + col) in mines)
+        cur_tile = Tile(row, col)
         cur_tile.label.grid(row=row, column=col)
         tiles[row].append(cur_tile)
 
@@ -134,8 +157,6 @@ win = tk.PhotoImage(file="dead.gif")
 
 avatar = tk.Label(status, image=fine)
 avatar.pack()
+avatar.bind('<Button-1>', restart_game)
 
-
-
-
-root.mainloop()
+restart_game()
