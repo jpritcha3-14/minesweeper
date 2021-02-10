5a3a27bfcff0c3e7139346befeb0bff0460eb154
Jonathan Pritchard <jpritcha3.14@gmail.com>
2021-02-09 22:09:41 -0800

    add check for neighboring bomb count

--- minesweeper.py
+++ minesweeper.py
@@ -2,49 +2,71 @@ import argparse
 import random
 
 import tkinter as tk
-from functools import partial
+from collections import deque
 
 # Size and number of bombs, to be replaced with parseargs
 sz = 20 
 b = 20
 
+# Frame setup
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
+tiles = [[] for _ in range(sz)]
+
 class Tile:
-    def __init__(self, isbomb):
-        self.isbomb = isbomb
+    def __init__(self, row, col, isbomb):
+        self.row = row
+        self.col = col
+        self.isbomb = isbomb 
         self.flagged = False
         self.clicked = False
 
         self.text = tk.StringVar()
         self.text.set(" ")
 
-        self.label = tk.Label(frame, relief="raised", textvariable=self.text, width=2, height=1)
+        self.label = tk.Label(frame, relief="groove", textvariable=self.text, width=2, height=1)
         self.label.bind("<Button-1>", self.left_click)
         self.label.bind("<Button-2>", self.right_click)
         self.label.bind("<Button-3>", self.right_click)
 
-    def left_click(self, event):
+    @staticmethod
+    def inbounds(tile, i, j):
+        return (    tile.row + i >= 0 
+                and tile.row + i < sz 
+                and tile.col + j >= 0 
+                and tile.col + j < sz)
+
+    def checkneighbors(self):
+        unclicked = deque([self])
+        while unclicked:
+            bombcount = 0
+            cur = unclicked.pop()
+            for i in [-1, 0, 1]:
+                for j in [-1, 0, 1]:
+                    if self.inbounds(cur, i, j):
+                        t = tiles[cur.row + i][cur.col + j]
+                        if t.isbomb:
+                            bombcount += 1
+            if bombcount > 0:
+                cur.text.set(str(bombcount))
+
+    def left_click(self, event=None):
         if not self.clicked and not self.flagged:
             self.clicked = True
-            self.text.set("b" if self.isbomb else "s")
-
-    def right_click(self, event):
-        if not self.clicked:
-            if not self.flagged:
-                self.flagged = True
-                self.text.set("f")
+            self.label.config(relief="flat")
+            if self.isbomb:
+                self.text.set("b")
             else:
-                self.flagged = False
-                self.text.set(" ")
+                self.text.set(" ")  
+                self.checkneighbors()
 
-# Map the tile row/col to the 1D set of bombs
-def checkbomb(r, c):
-    return (r * sz + c) in bombs
+    def right_click(self, event=None):
+        if not self.clicked:
+            self.flagged = not self.flagged
+            self.text.set("f" if self.flagged else " ")
 
-tiles = [[] for _ in range(sz)]
 
 # Create a random 1D set of bombs that maps to the 2D tile grid
 bombs = set()
@@ -54,7 +76,7 @@ while len(bombs) < b:
 # Create the tile grid
 for row in range(sz):
     for col in range(sz):
-        cur_tile = Tile(checkbomb(row, col))
+        cur_tile = Tile(row, col, (row * sz + col) in bombs)
         cur_tile.label.grid(row=row, column=col)
         tiles[row].append(cur_tile)
 
